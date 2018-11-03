76087d1cd19814a510f6e352ce8ad0b6a192d202
ShreyasDevadiga <41786558+ShreyasDevadiga@users.noreply.github.com>
2018-11-02 23:57:34 -0700

    New Customer page done

--- app/src/main/java/www/psycodes/in/carwash/HomePage.java
+++ app/src/main/java/www/psycodes/in/carwash/HomePage.java
@@ -1,13 +1,29 @@
 package www.psycodes.in.carwash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+
+    Button new_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        new_button = findViewById(R.id.new_button);
+
+    }
+    public void next(View view) {
+     Intent intent = new Intent(this,new_customer.class);
+     startActivity(intent);
+    }
+
+    public void search(View view) {
+        Intent intent = new Intent(this, search_customer.class);
     }
 }
