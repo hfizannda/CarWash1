c9276ee093169012fbb31475cc08e12d52cf7af0
SureshPrabhuParkala <sureshprabhuparkala@gmail.com>
2018-11-03 14:16:04 +0530

    home page reconstruction

--- app/src/main/java/www/psycodes/in/carwash/HomePage.java
+++ app/src/main/java/www/psycodes/in/carwash/HomePage.java
@@ -1,7 +1,10 @@
 package www.psycodes.in.carwash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
     }
 }
